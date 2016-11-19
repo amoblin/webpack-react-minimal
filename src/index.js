17490296977bcbf4be89d7813da915b7f038e5b3
amoblin@mbp <amoblin@gmail.com>
2016-11-19 09:40:36 +0800

    use Component class

--- src/index.js
+++ src/index.js
@@ -7,16 +7,18 @@ function Title(props) {
   return <h1>Hello, {props.name}</h1>
 }
 
-function App(props) {
-  return (
-    <div>
-      <NavBar leftContent="返回"
-              mode="light"
-              onLeftClick={() => console.log('onLeftClick')}
-              rightContent={[<Icon key="0" type="search" />, <Icon key="1" type="ellipsis" />]}
-      >NavBar</NavBar>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar leftContent="返回"
+                mode="light"
+                onLeftClick={() => console.log('onLeftClick')}
+                rightContent={[<Icon key="0" type="search" />, <Icon key="1" type="ellipsis" />]}
+        >NavBar</NavBar>
+      </div>
+    );
+  }
 }
 
-render(App(), document.getElementById('root'));
+render(<App/>, document.getElementById('root'));
